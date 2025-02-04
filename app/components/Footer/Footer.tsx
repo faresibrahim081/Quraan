a732042f53bb43326f019fec3bc293c3ceb01201
Fares ibrahim <faresibrahim081@gmail.com>
2025-02-04 16:23:35 +0200

    update the footer position

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
     return (
-        <footer className="bg-white mt-[5rem] py-5 text-center absolute bottom-0 left-0 w-full text-xl p-3">
+        <footer className="bg-white mt-[5rem] py-5 text-center w-full text-xl p-3">
             <h1 className="text-gray-800 font-bold">لا تنسو الدعاء لصديقي المتوفي   <span className="text-teal-600">حسام دياب</span> وجزانا الله واياكم كل خير</h1>
         </footer>
     )
