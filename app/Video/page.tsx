4954ad152c48a4965a54af0e5523b24b4a5168bd
Fares Ibrahim <faresibrahim081@gmail.com>
2026-02-16 20:39:29 +0200

    modify some ui and make and build for the domain deployment

--- app/Video/page.tsx
+++ app/Video/page.tsx
@@ -29,6 +29,7 @@ function Video() {
             }
         }
     };
+
     return (
         <div style={{ backgroundImage: `url(/images/bg-quran-2.webp)` }}
             className="mt-[4rem] relative bg-cover bg-center bg-no-repeat py-10">
