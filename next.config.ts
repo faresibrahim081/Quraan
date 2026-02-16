366967314658499b911764684e3542ca629900e7
Fares Ibrahim <faresibrahim081@gmail.com>
2026-02-16 20:49:45 +0200

    edit next.config.ts to fix the deployment problem

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,18 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   swcMinify: true,
-// };
+const nextConfig: NextConfig = {
+  swcMinify: true,
+};
 
-// export default nextConfig;
+export default nextConfig;
 
-import withBundleAnalyzer from '@next/bundle-analyzer';
+// import withBundleAnalyzer from '@next/bundle-analyzer';
 
-const analyzer = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-})
+// const analyzer = withBundleAnalyzer({
+//   enabled: process.env.ANALYZE === 'true',
+// })
  
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
  
-export default analyzer(nextConfig)
+// export default analyzer(nextConfig)
